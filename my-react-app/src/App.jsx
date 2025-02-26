08f918668cf66eff36ee787df2921b5289793661
Alishan Shaikh <rwbn1.alishan.as@gmail.com>
2025-02-26 17:10:24 +0530

    Update App.jsx

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,22 +1,49 @@
 
+
+import React, { useState } from 'react'
 import './App.css'
 import { Child } from './components/Child'
 import { Parent } from './components/Parent'
 
 function App() {
 
-  let obj = {
-    name: "red", 
-    sirname : "white", 
-    loc : "bhavngar"
+  const [count , setCount] = useState(0)
+  const [num , setNum] = useState(0);
+
+  const incCount = () =>{
+    setCount(count+1)
   }
 
-  let arr = ["first", "second" , "third", "fourth"]
+  const decCount = () =>{
+    setCount(count-1)
+  }
+
+
+  const handleCount = (n) =>{
+    setNum(num+n)
+  }
+
+
 
 return (
     <>
-       <Child props={obj} />
-       <Parent props = {arr} /> 
+        <button onClick={incCount}>INC = {count} {num}</button>
+        <button>{count}</button>
+        
+        <button onClick={decCount}>DESC = {count}</button>
+
+        <br/>
+        <hr/>
+        <br/>
+
+        <button onClick={() => handleCount(1)}>+</button>
+        <h3>{num}</h3>
+        <button onClick={() => handleCount(-1)}>+</button>
+
+        <br/>
+        <hr/>
+        <br/>
+  
     </>
   )
 }
