4e8d3b7637f4d7284102656d35a615736b43f690
alishaikh9890 <alishanshaikh9890@gmail.com>
2025-02-28 17:12:57 +0530

    hide password

---
+++ index.js
@@ -0,0 +1,27 @@
+
+
+let val1 = 10;
+let val2 = 20;
+
+function fun(num1, num2){
+
+    let result = num1 + num2;
+   return   fun2(result);
+  console.log(resutl)
+}
+
+function fun2(res){
+    return res*res
+
+    console.log(num1)
+}
+
+let result1 = fun(val1, val2);
+let result2 = fun(40, 70);
+let result3 = fun(40, 70);
+
+
+
+// global execution context
+
+// functinal execution context
--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -7,10 +7,24 @@ import { Props } from './props/Props'
 
 function App() {
 
+  const [show, setShow] = useState(true)
+
+  console.log(show)
 
 
 return (
     <>
+    <div className='border p-5'>
+      <input type={show ? "password" : "text"} />
+      <button className='btn btn-sm ' style={{transform:"translateX(-100%)"}} onClick={ () => setShow(!show) }>
+       {
+        !show ?
+        <i class="fa-solid fa-eye"></i>
+        :
+        <i class="fa-solid fa-eye-slash"></i>
+       } 
+        </button>
+    </div>
       {/* <Parent /> */}
       <Props/>
     </>
