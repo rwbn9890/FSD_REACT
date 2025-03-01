c65e8e7738e9b8fa624039a6f403bdb26c436154
alishaikh9890 <alishanshaikh9890@gmail.com>
2025-03-01 15:14:15 +0530

    final push

--- index.js
+++ index.js
@@ -3,22 +3,34 @@
 let val1 = 10;
 let val2 = 20;
 
-function fun(num1, num2){
+
+function fun(num1, num2){  // lexical scope
 
     let result = num1 + num2;
-   return   fun2(result);
-  console.log(resutl)
+
+    return fun2(result);
 }
+
+check()
+
+check2()
 
 function fun2(res){
-    return res*res
-
-    console.log(num1)
+    return res*res;
 }
 
-let result1 = fun(val1, val2);
-let result2 = fun(40, 70);
-let result3 = fun(40, 70);
+function check(){
+
+}
+
+
+function check2(){
+
+}
+
+
+let result1 = fun(val1, val2); // fun2()
+let result2 = fun(40, 30);
 
 
 
