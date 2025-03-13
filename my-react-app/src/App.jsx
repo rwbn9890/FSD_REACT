f908b235f7e2d6149d564dbe406984a5d8dbbbb8
alishaikh9890 <alishanshaikh9890@gmail.com>
2025-03-13 17:15:39 +0530

    json-server intro

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,32 +1,83 @@
 
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
-// import { Child } from './components/Child'
-import  Parent from './components/Parent'
-import { Props } from './props/Props'
+
 
 function App() {
+const [data, setData] =useState([])
+const [loading, setLoading] = useState(false)
+const [error, setError] = useState(null)
 
-  const [show, setShow] = useState(true)
 
-  console.log(show)
+
+let prod_url= `http://localhost:8000/prod`
+let users_url = `http://localhost:8000/users/`
+
+
+
+  const fetchAndUpdateData = async (url, obj) =>{
+    setLoading(true);
+   try { 
+      const res = await fetch(url, obj);
+      const prod = await res.json();
+      setData(prod)
+   } catch (error) {
+      setError(error)
+      console.log("message: " + error)
+   } finally {
+    setLoading(false)
+   }
+  }
+
+  useEffect(() => {
+    fetchAndUpdateData(users_url, {})
+  }, [])
+
+  console.log(data)
+
+
+
+  const delProduct = (id) => {
+
+    const itemId = 4;
+const url = `http://localhost:8000/users/${+itemId}`;
+
+      fetch(url, {
+        method: "DELETE"
+      }).catch(err => {
+        console.log(err)
+      })
+  }
 
 
 return (
     <>
-    <div className='border p-5'>
-      <input type={show ? "password" : "text"} />
-      <button className='btn btn-sm ' style={{transform:"translateX(-100%)"}} onClick={ () => setShow(!show) }>
-       {
-        !show ?
-        <i class="fa-solid fa-eye"></i>
-        :
-        <i class="fa-solid fa-eye-slash"></i>
-       } 
-        </button>
+    <div className="container">
+      <div className="row">
+        { 
+        loading ? (
+          <h2>loading...!</h2>
+        ): error ?  (
+          <h2>Something went wrong</h2>
+        ) : (
+           data.map((ele) =>(
+            <div key={ele.id} className="col-3">
+            <div className="card">
+              <img src={ele.images[0]} alt="" className="card-img-top" />
+              <div className="card-body">
+                <h5 className="card-title">{ele.title}</h5>
+                <p className="card-content"></p>
+
+                <button className="btn btn-light" onClick={() => delProduct(ele.id)}>Delete</button>
+              </div>
+            </div>
+          </div>
+          ))
+        )
+         
+        }
+      </div>
     </div>
-      {/* <Parent /> */}
-      <Props/>
     </>
   )
 }
--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 
 
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.bundle.js"
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
